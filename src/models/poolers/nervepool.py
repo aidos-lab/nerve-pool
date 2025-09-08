f2935dd6417949fdb3c6017251bca7203bc50758
Ernst Röell <ernstroell@gmail.com>
2025-09-08 14:06:58 +0200

    cleaning

--- src/models/poolers/nervepool.py
+++ src/models/poolers/nervepool.py
@@ -58,7 +58,9 @@ def nerve_pool_complex(
         1, batch_size
     ) + num_virtual_nodes * torch.arange(batch_size, device=device).repeat_interleave(
         virtual_edge_index.shape[1]
-    ).unsqueeze(0)
+    ).unsqueeze(
+        0
+    )
 
     return (
         pooled_features[:, :num_virtual_nodes, :].reshape(-1, node_features.shape[1]),
@@ -68,47 +70,3 @@ def nerve_pool_complex(
             torch.arange(batch_size, device=device), num_virtual_nodes
         ),
     )
-
-
-# def nerve_pool_complex(
-#     node_features,
-#     edge_features,
-#     edge_index,
-#     cluster_assignments,
-# ):
-#     device = node_features.device
-#     num_virtual_nodes = cluster_assignments.shape[1]
-#
-#     # Initialize
-#     virtual_edge_index = torch.tensor(
-#         list(
-#             itertools.product(
-#                 range(num_virtual_nodes),
-#                 range(num_virtual_nodes),
-#             )
-#         ),
-#         device=device,
-#     ).T
-#
-#     s = cluster_assignments
-#
-#     # Down Function
-#     s_edges_virtual_nodes = cluster_assignments[edge_index].max(dim=0)[1]
-#     s = torch.vstack([s, s_edges_virtual_nodes])
-#
-#     # Right function (naming convention by)
-#     s_all_virtual_edges = s[:, virtual_edge_index].min(dim=1)[1]
-#     non_empty_mask = s_all_virtual_edges.abs().sum(dim=0).bool()
-#     s_all_virtual_edges = s_all_virtual_edges[:, non_empty_mask]
-#
-#     s = torch.hstack([s, s_all_virtual_edges])
-#
-#     features = torch.vstack([node_features.to(device), edge_features.to(device)])
-#
-#     pooled_features = s.T @ features
-#
-#     return (
-#         pooled_features[:num_virtual_nodes],
-#         pooled_features[num_virtual_nodes:],
-#         virtual_edge_index[:, non_empty_mask],
-#     )
